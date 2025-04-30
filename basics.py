576843bd619804678930927890e4f5e4fd6db22c
beata-teck <batatefera7@gmail.com>
2025-04-30 15:28:25 -0800

    Add basics.py

---
+++ basics.py
@@ -0,0 +1,20 @@
+#practice basic python concepts
+
+#variable
+name="beata"
+age=21
+ 
+#if/else
+if age>18:
+print(name,"is an adult")
+else:
+print(name, "is a minor")
+
+#Loops
+for i in range(5):
+print("Loop number:" ,i)
+
+#Function
+def greet(person):
+print("Hello", person)
+ greet(name)
