49b041b70dd6378f1552468b7a1a148acd658510
beata-teck <batatefera7@gmail.com>
2025-04-30 16:10:48 -0800

    Add data_structures.py with lists , dicts, and inputs

---
+++ data_structures.py
@@ -0,0 +1,16 @@
+#List
+fruits=["apple","banana","cherry"]
+print("First fruits:" , fruits[0])
+print("All fruit" , fruits)
+#Dictionaries
+Student = {
+	"name":"beata",
+	"age":21,
+	"Major":"software Engineering"
+}
+print("student's name:",
+Student ["name"])
+print("Full info:",Student)
+#input
+color= input("What is your favorite color?")
+print("Nice choice !" , color)
